3ae4c82738288c5f9426388680769b67f17fc168
kayyenl <yenyenlee9@gmail.com>
2023-06-02 16:04:04 +0800

    first commit hope its ok

--- App.js
+++ App.js
@@ -11,8 +11,8 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  container: { 
+    flex: 1, 
     backgroundColor: '#fff',
     alignItems: 'center', 
     justifyContent: 'center',  
